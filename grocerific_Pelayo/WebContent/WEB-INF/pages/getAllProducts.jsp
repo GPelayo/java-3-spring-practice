5ca9ac9519bc4fc425e6fa3e9ecb4a6ed556e5bf
gpelayo109@gmail.com <gpelayo109@gmail.com@68527fb0-f798-9f78-cf47-3d07915ec97b>
2013-06-26 13:29:10 +0000

    Added CSS and Background Design. Added Incomplete Editing Functionality.

--- grocerific_Pelayo/WebContent/WEB-INF/pages/getAllProducts.jsp
+++ grocerific_Pelayo/WebContent/WEB-INF/pages/getAllProducts.jsp
@@ -1,72 +1,33 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt"  prefix="fmt" %>
 <%@ page session="false"%>
 <html>
 <head>
-<title>Home</title>
-	<style>
-		body
-		{
-			color: rgb(142, 87, 86);
-			background-image:url("http://farm4.static.flickr.com/3570/3333105812_4f062174d1.jpg");
-		}		
-		h1
-		{
-			margin:0
-			
-		}
-		div.header
-		{
-			text-align:center;
-			background-color: rgb(135, 216, 222);
-			border-width:5;
-			border-style:solid;
-			border-color: rgb(88, 133, 52);			
-			background-color: rgb(135, 216, 222);
-			margin-bottom: 10px;
-			max-width: 33%;
-			margin-left: 33%;
-			margin-right: 33%;
-		}
-		table
-		{
-			float:left;
-			width:%40;
-			border-width:5;
-			border-style:solid;
-			border-color: rgb(88, 133, 52);			
-			background-color: rgb(135, 216, 222);
-		}
-		td
-		{
-			border-bottom-width:3;
-			border-bottom-style:solid;
-			border-bottom-color: rgb(138, 183, 102);
-		}
-	</style>
+	<title>Grocerific Product List</title>
+	<link rel="stylesheet" type="text/css" href="/grocerific_Pelayo/RES/css/list.css"/>
 </head>
 <body>
 
-<div class="header"><h1>Product Inventory</h1></div>
+<div class="header" style="margin-bottom: 10px;"><h1>Product Inventory</h1></div>
 <table>
-	<tr><td>
-		<tr style="font-weight:bold">
-			<td>ID</td>
-			<td>Description</td>
-			<td>Size</td>
-			<td>Price</td>
-			<td/><td/>
+	<tr style="font-weight:bold; text-align:center" >
+		<td>ID</td>
+		<td>Description</td>
+		<td>Size</td>
+		<td>Price</td>
+		<td colspan="2">Action<td>
+	</tr>
+	<c:forEach items="${products}" var="iProduct">
+		<tr>
+			<td>${iProduct.id}</td>
+			<td>${iProduct.description}</td>
+			<td>${iProduct.size}</td>
+			<td>${iProduct.price}</td>
+			<td><a href="editProduct?id=${iProduct.id}">Edit</a></td>
+			<td><a href="deleteProduct?id=${iProduct.id}">Delete</a></td>
 		</tr>
-		<c:forEach items="${products}" var="iProduct">
-			<tr>
-				<td>${iProduct.id}</td>
-				<td>${iProduct.description}</td>
-				<td>${iProduct.size}</td>
-				<td>${iProduct.price}</td>
-				<td><a href="/grocerific_Pelayo/editProduct?id=${iProduct.id}">Edit</a></td>
-				<td><a href="/grocerific_Pelayo/deleteProduct?id=${iProduct.id}">Delete</a></td>
-			</tr>
-		</c:forEach>
-	<td></tr>
+	</c:forEach>	
 </table>
 </body>
 </html>
--- grocerific_Pelayo/src/com/grocerific/controller/ProductController.java
+++ grocerific_Pelayo/src/com/grocerific/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.grocerific.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,49 +13,55 @@ import org.springframework.web.servlet.mvc.AbstractController;
 import com.grocerific.product.*;
 
 @Controller
-public class ProductController extends AbstractController{
-	ProductJDBC productJdbc;
-	
-	@RequestMapping(method = RequestMethod.GET, value = "/wew")
-	public String testThingy()
+public class ProductController extends AbstractController{	
+	/*
+	@RequestMapping(value = "resource/css/style.css")
+	public String showYoStylin()
 	{
-		return "redirect:/products/list";
+		System.out.println("CSS access attempted");
+		return "resource/css/style.css";
 	}
+	*/
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/products/list")
 	public ModelAndView listProducts(){
+		ProductJDBC productJdbc = (ProductJDBC)getApplicationContext().getBean("productJDBCTemplate");
 		System.out.println("List Accessed");
-		productJdbc = (ProductJDBC)getApplicationContext().getBean("productJDBCTemplate");
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/getAllProducts");
 		mav.addObject("products", productJdbc.listProducts());
 		return mav;
 	}
 
-	@RequestMapping(method =RequestMethod.GET, value = "/editProduct")
-	public ModelAndView editProduct(@RequestParam(value="id") Integer id)
-	{		
-		productJdbc = (ProductJDBC)getApplicationContext().getBean("productJDBCTemplate");
-		Product product = productJdbc.getProductById(id);
-		
-		ModelAndView productModelView = new ModelAndView();
-		productModelView.setViewName("editProduct");
-		productModelView.addObject("id", product.getId());
-		productModelView.addObject("description", product.getDescription());
-		productModelView.addObject("size", product.getSize());
-		productModelView.addObject("price", product.getPrice());
-		
-		System.out.print(product.toString());
-		return productModelView;
+	@RequestMapping(method=RequestMethod.POST, value="/products/list") 
+	public String saveProduct(@RequestParam(value="productId") Integer id, @ModelAttribute Product product){
+		product.setId(id);
+		ProductJDBC productJdbc = (ProductJDBC)getApplicationContext().getBean("productJDBCTemplate");
+		productJdbc.update(product);		
+		return "list";
 	}
-
-	@RequestMapping(method=RequestMethod.POST,value="edit") 
-	public String savePerson(@ModelAttribute Product product){
-		return "It works";
+	
+	@RequestMapping(method =RequestMethod.GET, value = "/products/editProduct")
+	public ModelAndView editProduct(@RequestParam(value="id") Integer id, @ModelAttribute("edit-product") Product productToBeEdited)
+	{
+		ProductJDBC productJdbc = (ProductJDBC)getApplicationContext().getBean("productJDBCTemplate");
+		productToBeEdited = productJdbc.getProductById(id);
+		return initializeProductModel(productToBeEdited, "editProduct");
 	}
 
 	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 		return null;
 	}
-
+	
+	private ModelAndView initializeProductModel(Product product, String viewName)
+	{
+		ModelAndView productModelView = new ModelAndView();
+		productModelView.setViewName(viewName);
+		productModelView.addObject("productId", product.getId());
+		productModelView.addObject("description", product.getDescription());
+		productModelView.addObject("size", product.getSize());
+		productModelView.addObject("price", product.getPrice());
+		
+		return productModelView;
+	}
 }
